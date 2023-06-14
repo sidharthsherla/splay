7d2d4e2a5a91a9eb4dfa1eeeb0a322a4ee378f67
SidharthSherla <101195485+sidharthsherla@users.noreply.github.com>
2023-06-14 14:10:22 +0530

    Update script.js

--- splay2/script.js
+++ splay2/script.js
@@ -1,14 +1,14 @@
-// List videos from local directory
+
 function listVideos() {
     var videosContainer = document.getElementById('videos-container');
   
-    // Array of video file names
+    
     var videoFiles = [
       'video1.mp4',
       'video2.mp4',
       'video3.mp4',
       'video4.mp4'
-      // Add more video file names as needed
+      
     ];
   
     videoFiles.forEach(function(videoFile) {
@@ -22,12 +22,12 @@ function listVideos() {
     });
   }
   
-  // Play video by file name
+ 
   function playVideo(videoFile) {
-    var videoUrl = 'videos-container/' + videoFile; // Replace with the path to your video files
+    var videoUrl = 'videos-container/' + videoFile; 
     window.open(videoUrl);
   }
   
-  // Call the function to list the videos
+  
   listVideos();
   
